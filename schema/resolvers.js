4fc1f3580c2b7f2b1b93575c1c0f966260234f77
Cameron Chardukian <camchardukian@gmail.com>
2022-02-06 11:50:16 -0600

    feature/additional-resolvers-and-enums

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,9 +1,34 @@
-const { UserList } = require("../FakeData");
+const { UserList, MovieList } = require("../FakeData");
+const _ = require("lodash");
 
 const resolvers = {
   Query: {
-    users() {
+    // USER RESOLVERS
+    users: () => {
       return UserList;
+    },
+    user: (parent, args) => {
+      const id = args.id;
+      const user = _.find(UserList, { id: Number(id) });
+      return user;
+    },
+    // MOVIE RESOLVERS
+    movies: () => {
+      return MovieList;
+    },
+    movie: (parent, args) => {
+      const name = args.name;
+      const movie = _.find(MovieList, { name });
+      return movie;
+    },
+  },
+  User: {
+    favoriteMovies: () => {
+      return _.filter(
+        MovieList,
+        (movie) =>
+          movie.yearOfPublication >= 2010 && movie.yearOfPublication <= 2020
+      );
     },
   },
 };
